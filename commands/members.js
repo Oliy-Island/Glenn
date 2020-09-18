75338ab1a2acec6faf9d908800bf0a9cb022d3dc
Milan Mehra <keiki08@outlook.com>
2020-09-17 23:45:43 -0500

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -1,6 +1,3 @@
 module.exports.run = async function () {
     return this.reply(`**Member Count:** ${message.guild.members.cache.filter((m) => !m.bot).size.toLocaleString()}`);
 }
-
-module.exports.info = {
-}
