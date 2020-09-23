34358aea0315b7e0d252851ca4177a6bd864cdec
Luke Stoodley <luke@ichigo.uk>
2020-09-23 09:31:27 +0100

    Update CommandContext.js

--- structs/CommandContext.js
+++ structs/CommandContext.js
@@ -12,7 +12,7 @@ class CommandContext {
   reply (text) {
     return this.send(this.client.embed
       .setColor('GREEN') // TODO
-      .setFooter(msg.author.tag, msg.author.avatarURL())
+      .setFooter(this.message.author.tag, this.message.author.avatarURL())
       .setDescription(this.client.replacer(text, {
         user: `${this.message.author}`,
         channel: `${this.message.channel}`
@@ -24,7 +24,7 @@ class CommandContext {
   error (text) {
     return this.send(this.client.embed
       .setColor('RED')
-      .setFooter(msg.author.tag, msg.author.avatarURL())
+      .setFooter(this.message.author.tag, this.message.author.avatarURL())
       .setDescription(`${text ? `Error Occured: ${text}` : 'An Error Occured While Running This Command'}`)
       .setTimestamp()
     )
