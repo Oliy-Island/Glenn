436859b8ec6c3e73da7c95d346fce9dd166363d1
Shade <me@shadeoxide.gq>
2020-09-26 08:59:57 +0530

    Updated Avatar Command
    Added some stuff so that it won't be necessary everytime to ping someone or use ID.
    Now you can use the command like !avatar Oliy

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,5 +1,5 @@
 exports.run = async function (message) {
-  const user = message.mentions.users.first() || message.author
+  const user = message.mentions.users.first() || this.client.users.cache.find(u => u.username.toLowerCase().startsWith(message.args.slice(0).join(" ").toLowerCase())) || message.author
 
   this.send(this.client.embed
     .setImage(user.displayAvatarURL({ dynamic: true, size: 2048, format: 'png' }))
